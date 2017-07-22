3f91fc2f95d45437a4c32569450487ef8238bbfb
Gabriel Morales <heyitsgabe@users.noreply.github.com>
2017-07-21 22:48:10 -0400

    Converted text to binary added read me file

---
+++ CrcCheck/README.txt
@@ -0,0 +1,5 @@
+Compilitation command: javac crcheck.java
+
+run command: java crcheck
+
+This program crcheck.java is entirely my own work and I have not developed my program in conjunction with another person nor copied program code from any other person, nor permitted your code to be copied or otherwise used by any other person, nor have you copied, modified, or otherwise used program code that you have found in any external source, including but not limited to, online sources.
--- CrcCheck/src/crcheck.java
+++ CrcCheck/src/crcheck.java
@@ -1,54 +1,65 @@
 import java.awt.DisplayMode;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+ * CRC polynomial: x^16 + x^10 + x^8 + x^7 + x^3 + 1 = 0x0589
+ *  
+ * only two parameters can be passed:
+ * 
+ * Parameter 1: "c" or "v" c = calculate || v = verify -if parameter one
+ * isn't the above, print error message and exit
+ * 
+ * Parameter 2: Name of file file needs a be a text file in the same folder
+ * as program if file isn't located in same folder, print error and exit
+ * 
+ * 
+ */
 
 public class crcheck {
 
-	/*
-	 * CRC polynomial: x^16 + x^10 + x^8 + x^7 + x^3 + 1 = 0x0589
-	 *  
-	 * only two parameters can be passed:
-	 * 
-	 * Parameter 1: "c" or "v" c = calculate || v = verify -if parameter one
-	 * isn't the above, print error message and exit
-	 * 
-	 * Parameter 2: Name of file file needs a be a text file in the same folder
-	 * as program if file isn't located in same folder, print error and exit
-	 * 
-	 * source file must be named crcheck.java
-	 * 
-	 * include README file that contains 
-	 * • The compilation command for your program 
-	 * • The run command for your program 
-	 * • Your statement that the
-	 * 	program is entirely your own work and that you have neither developed
-	 * 	your code together with any another person, nor copied program code from
-	 * 	any other person, nor permitted your code to be copied or otherwise used
-	 * 	by any other person, nor have you copied, modified, or otherwise used
-	 * 	program code that you have found in any external source, including but
-	 * 	not limited to, online sources.
-	 */
+	// variables
+	private static String testText = "TestCase.txt";
+	private static String[] textArray;
+	private static int POLYNOMIAL_CHECK = 0x0589; //crc calculation for polynomial
+	private static Scanner scanner;
+	private static List textLines;
 
-	public static void main(String[] args) throws IOException {
+	public crcheck() {
+	}
+
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
-		
-		displayFiletext();
+		System.out.println("CRC Check... \n");
+
+		try {
+			displayFileText();
+			System.out.println("\n");
+			textArray();
+			System.out.println("\n");
+			xor();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 
 	}
-	
-	public static  void displayFiletext() throws IOException{
-		
-		InputStream input = new BufferedInputStream(new FileInputStream("TestCase.txt"));
-		
-		byte[] buffer = new byte[8192];
-		
+
+	public static void displayFileText() throws IOException {
+
+		InputStream input = new BufferedInputStream(new FileInputStream(testText));
+
+		byte[] buffer = new byte[512];
+
 		try {
 			for (int length = 0; (length = input.read(buffer)) != -1;) {
 				System.out.write(buffer, 0, length);
@@ -56,25 +67,53 @@ public class crcheck {
 		} finally {
 			input.close();
 		}
-		
-}
-
-class Calculations {
-	
-	// variables
-	int POLYNOMIAL_CHECK = 0x0589;
-	
-	// constructor method
-	public Calculations() {	}
-	
-	// calculate method
-	public void calculate() {
-		
 	}
-
-	// verify method
-	public void verify() {
+	
 		
-		}	
+	public static void textArray() throws FileNotFoundException {
+		scanner = new Scanner(new File(testText));
+		List<String> textLines = new ArrayList<String>();
+		
+		try {
+			while (scanner.hasNextLine()) {
+				textLines.add(scanner.nextLine());
+			}
+		} catch (Exception ex) {
+			
+		}
+		
+		textArray = textLines.toArray(new String[0]);
+		System.out.println(textArray);
 	}
+		public static void xor() {
+		/*
+		 * array1 will be equal to the polynomial used as the divisor
+		 * array2 will be used as the dividend
+		 * 
+		 * array3 will be the dividend with remainder as padding 
+		 */
+			
+			byte[] byteArray = testText.getBytes();
+			StringBuilder bin = new StringBuilder();
+			
+			for (byte b : byteArray) {
+				int value = b;
+				for (int i = 0; i < 8; i ++) {
+				bin.append((value & 128) == 0 ? 0 : 1);
+				value <<=1;
+				}
+				bin.append(' ');
+			}
+			System.out.println("test case in binary: " + bin);
+			
+		}
+		
+		/*
+		 * result += Integer.tobinaryString((int)c);
+		 * 
+		 * String bString = Integer.toBinaryString((int)c);
+		 * bResult += ("00000000" + bString).substring(bString.length());
+		 * 
+		 * this command should pad the end of the lines with 8 0's during the calculation process
+		 */
 }
